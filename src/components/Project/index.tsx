a7edf23f2731f69453bbd746946245a664b07883
Felipe Silva <felipe.silva@kenzie.com.br>
2023-07-17 08:54:26 -0300

    update project html_url

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -16,7 +16,7 @@ interface ReposType {
   name: string;
   language: string;
   description: string;
-  git_url: string;
+  html_url: string;
   homepage: string;
 }
 
@@ -70,7 +70,7 @@ export const Project = (): JSX.Element => {
             {repository.description}
           </Text>
           <ProjectLinks>
-            <ProjectLink target="_blank" href={repository.git_url}>
+            <ProjectLink target="_blank" href={repository.html_url}>
               <FaGithub /> Github Code
             </ProjectLink>
             {repository.homepage && (
